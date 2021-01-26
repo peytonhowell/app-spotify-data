73827f6523ab35d8a0feeea12d0bcf05eeddd502
peytonhowell <peytonhowelle@gmail.com>
2021-01-26 00:11:30 -0500

    push lol

--- src/components/SpotifyInformation/SpotifyInformation.js
+++ src/components/SpotifyInformation/SpotifyInformation.js
@@ -11,9 +11,10 @@ export class SpotifyInformation extends React.Component {
       songs: []
     }
   }
-  componentDidMount() {
+  componentDidMount(props) {
     this.getArtists();
     this.getSongs();
+    this.getUser();
   }
   getArtists = async() => {
     var artists = []; 
@@ -26,6 +27,7 @@ export class SpotifyInformation extends React.Component {
       }).then(results => results.json()).then(results => artists = results.items);
     }
     this.setState({artists: artists})
+
   }
 
   getSongs = async() => {
@@ -41,22 +43,53 @@ export class SpotifyInformation extends React.Component {
     this.setState({songs: songs})
   }
 
+  getUser = async() => {
+    var profile = []; 
+    if (this.props.token) {
+      await fetch('https://api.spotify.com/v1/me', {
+        method: 'GET',
+        headers: {
+            'Authorization': 'Bearer ' + this.props.token,
+        }
+      }).then(results => results.json()).then(results => profile = results);
+    }
+    this.setState({profile: profile})
+  }
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
   render() {
       return(
         <div className="flexRow">
           <div className="flexColumn">  
             <h2>Top Artists</h2>
+            <ol className="leftAlign">
             {this.state.artists.length !== 0 ? this.state.artists.map((x) => (
-              <p>{x.name}</p>
+              <li>{x.name}</li>
             )): <p>No artists yet, please log in</p>}
+            </ol>
+
           </div>
           <div className="flexColumn">
             <h2>Top Songs</h2>
-            {this.state.songs.length !== 0 ? this.state.songs.map((x) => (
-              <p>{x.name}</p>
-            )): <p>No Songs, please log in</p>}
+            <ol className="leftAlign">
+              {this.state.songs.length !== 0 ? this.state.songs.map((x) => (
+                <li>{x.name}</li>
+              )): <p>No Songs, please log in</p>}
+            </ol>
           </div>
-
+          <div className="flexColumn">
+            <h2>User Info</h2>
+            <form className="leftAlign">
+              <label>
+                Location:  
+                <input type="text" name="name" />
+              </label>
+              <br></br>
+              <input type="submit" value="Submit" />
+            </form>
+          </div>
         </div>
       )
     }
